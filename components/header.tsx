c365260a92562a90b6abb7f92a8f3d60d2b0b6aa
Oliver <me@example.com>
2023-10-11 09:56:21 +0800

    added hover on header link

--- components/header.tsx
+++ components/header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <header className='fixed top-0 border-b border-gray-800 border-opacity-40 h-[4.5rem] w-full shadow-md shadow-gray-700/[0.05] backdrop-blur-[0.2rem]'>
         <nav className='flex justify-between items-center h-full mx-[2rem] sm:mx-[4rem]'>
-            <Link href="/" className={buttonVariants()}>
+            <Link href="/" className={`${buttonVariants()} hover:bg-gray-800`}>
               <h1>Primus</h1>
             </Link>
             <h1>Sign-in</h1>
