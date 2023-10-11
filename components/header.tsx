e79696f3319fa3cc9742618f3d87c786f754903b
Oliver <me@example.com>
2023-10-11 11:34:31 +0800

    Added background Image

--- components/header.tsx
+++ components/header.tsx
@@ -5,10 +5,13 @@ import { buttonVariants } from './ui/button'
 export default function Header() {
   return (
     <header className='fixed top-0 border-b border-gray-800 border-opacity-40 h-[4.5rem] w-full shadow-md shadow-gray-700/[0.05] backdrop-blur-[0.2rem]'>
-        <nav className='flex justify-between items-center h-full mx-[2rem] sm:mx-[4rem]'>
-            <Link href="/" className={`${buttonVariants()} hover:bg-gray-800`}>
-              <h1>Primus</h1>
-            </Link>
+        <nav className='flex justify-between items-center h-full mx-[2rem] sm:mx-[4rem] '>
+            <div className='flex items-center'>
+              <Link href="/" className={`${buttonVariants()} opacity-50 hover:bg-gray-700`}>
+                <h1>Logo</h1>
+              </Link>
+              <h1 className='ml-[1rem]'>Primus Private Security Training Institute</h1>
+            </div>
             <h1>Sign-in</h1>
         </nav>
         
